8e13f982c10d0842427e1cb466c5f3c974611745
Aleksandr60rus <113985770+Aleksandr60rus@users.noreply.github.com>
2023-05-20 00:46:43 +0300

    DidPagination+AddedUseContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,15 @@ import Sort from "../components/Sort";
 import Categories from "../components/Categories";
 import PizzaBlock from "../components/PizzaBlock";
 import Sceleton from "../components/PizzaBlock/Sceleton";
+import Pagination from "../components/Pagination";
+import { AppContext } from "../App";
 
 const Home = () => {
+  const { searchValue } = React.useContext(AppContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: "Популярности",
     sortProperty: "rating",
@@ -19,9 +23,9 @@ const Home = () => {
     const sortBy = sortType.sortProperty.replace("-", "");
     const order = sortType.sortProperty.includes("-") ? "asc" : " desc";
     const category = categoryId > 0 ? `category = ${categoryId}` : "";
-
+    const search = searchValue ? `search = ${searchValue}` : "";
     fetch(
-      `https://6460b48efe8d6fb29e35726a.mockapi.io/objects?${category}&sortBy=${sortBy}&order=${order}`
+      `https://6460b48efe8d6fb29e35726a.mockapi.io/objects?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -29,7 +33,11 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+  const sceletons = [...new Array(10)].map((_, indexLoading) => (
+    <Sceleton key={indexLoading} />
+  ));
+  const pizzas = items.map((obj) => <PizzaBlock {...obj} />);
   return (
     <div className="container">
       <div className="content__top">
@@ -41,13 +49,8 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
 
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(10)].map((_, indexLoading) => (
-              <Sceleton key={indexLoading} />
-            ))
-          : items.map((obj) => <PizzaBlock {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? sceletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
