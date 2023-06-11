9de82d2318259e70c57db2ab5d5b944f1e95a8d7
Aleksandr60rus <113985770+Aleksandr60rus@users.noreply.github.com>
2023-06-11 16:45:31 +0300

    second save

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import qs from "qs";
 import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { sortList } from "../components/Sort";
 import Sort from "../components/Sort";
